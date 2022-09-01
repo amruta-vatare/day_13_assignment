da6c4540cb8e60f006acbf09ed4d6ebcbe1cd601
amruta.vatare <vatareamruta615@gmail.com>
2022-09-01 13:27:44 +0530

    UC 2 implemented

--- com/bridgeLabs/FindMaximum.java
+++ com/bridgeLabs/FindMaximum.java
@@ -2,20 +2,37 @@ package com.bridgeLabs;
 
 public class FindMaximum {
     //Test Case - 1
-    public static void findMaxInt(Integer n1,Integer n2,Integer n3){
+    public int findMaxInt(Integer n1,Integer n2,Integer n3){
         if(n1.compareTo(n2)>0 && n1.compareTo(n3)>0){
-            System.out.println(n1+" is greater");
+            return n1;
         }else if(n2.compareTo(n1) >0 && n2.compareTo(n3)>0){
-            System.out.println(n2+" is greater");
-        }else if(n3.compareTo(n1)>0 && n3.compareTo(n2)>0){
-            System.out.println(n3+" is greater");
+            return n2;
+        }else{
+            return n3;
+        }
+    }
+    //Test Case - 2
+    public float findMaxFloat(Float fNum1, Float fNum2, Float fNum3){
+        if(fNum1.compareTo(fNum2)>0 && fNum1.compareTo(fNum3)>0){
+            return fNum1;
+        }else if(fNum2.compareTo(fNum1)>0 && fNum2.compareTo(fNum3)>0){
+            return fNum2;
+        }else {
+            return fNum3;
         }
     }
     public static void main(String[] args) {
+        FindMaximum maximum = new FindMaximum();
         Integer num1 = 100;
         Integer num2 = 99;
         Integer num3 = 50;
-        findMaxInt(num1,num2,num3);
+        int maxInt = maximum.findMaxInt(num1,num2,num3);
+        System.out.println("Maximum Integer Value is "+maxInt);
+        Float fNum1 = 19.20f;
+        Float fNum2 = 5.0f;
+        Float fNum3 = 39.00f;
+        float maxFloat = maximum.findMaxFloat(fNum1,fNum2,fNum3);
+        System.out.println("Maximum Float Value is "+maxFloat);
 
 
 
